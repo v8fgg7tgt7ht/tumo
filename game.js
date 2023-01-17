95cab16c2c97130bd5f7fe817e81c567ca9ab5f9
kdkljdkjf <splm337@gmail.com>
2023-01-17 13:08:08 +0200

    format

--- game.js
+++ game.js
@@ -1,73 +1,67 @@
-
-
-function setup(){
-    createCanvas(500,500);
+function setup() {
+    createCanvas(500, 500);
     Game.addCommonBalloon()
 }
 
-function draw(){
+function draw() {
 
     background('skyblue')
 
     for (let balloon of Game.balloons) {
-        
+
         balloon.display()
         balloon.move(Game.score);
-        
-        if(balloon.y <= balloon.size / 2 && balloon.color != 'black'){
-           
-    let Prompt = prompt("choose 1 or 2")
-  if (Prompt === "1") {
-    console.log("You've chosen 1");}
 
-let ot = Math.random();
+        if (balloon.y <= balloon.size / 2 && balloon.color != 'black') {
 
+            let Prompt = prompt("choose 1 or 2")
+            if (Prompt === "1") {
+                console.log("You've chosen 1");
+            }
 
-
-if (ot < 0.5) {
-    ot = "1";
-} else {
-    ot = "2";
-}
-
-console.log("random number is " + ot + ".");
-
-if (Prompt === ot) {
-    console.log('You win');
-    Game.balloons.length = 0 
-    continue
-   
-} else {
-    
-   console.log('You lose');
-    gameOver()
-    
-}
+            let ot = Math.random();
 
 
 
+            if (ot < 0.5) {
+                ot = "1";
+            } else {
+                ot = "2";
+            }
+
+            console.log("random number is " + ot + ".");
+
+            if (Prompt === ot) {
+                console.log('You win');
+                Game.balloons.length = 0
+                continue
+
+            } else {
+
+                console.log('You lose');
+                gameOver()
+
+            }
         }
     }
 
-   
-
     textSize(32)
     fill('black')
-    text(Game.score,20,40)
+    text(Game.score, 20, 40)
 
-    if (frameCount % 50 === 0){
+    if (frameCount % 50 === 0) {
         Game.addCommonBalloon()
     }
-    if(frameCount % 100 === 0 ){
+    if (frameCount % 100 === 0) {
         Game.addUniqBalloon()
     }
-    if(frameCount % 120 === 0 ){
+    if (frameCount % 120 === 0) {
         Game.addAngryBalloon()
     }
 }
 
-function mousePressed(){
-    if(!isLooping()){
+function mousePressed() {
+    if (!isLooping()) {
         loop()
         Game.score = 0;
     }
@@ -75,86 +69,87 @@ function mousePressed(){
 }
 
 class Game {
-    
-     static highScores = []
-    static balloons =[]
+
+    static highScores = []
+    static balloons = []
     static score = 0;
-    
-    
-    static addCommonBalloon(){
+
+
+    static addCommonBalloon() {
         let commonBalloon = new CommonBalloon('blue', 50)
         this.balloons.push(commonBalloon)
     }
 
-    static addUniqBalloon(){
+    static addUniqBalloon() {
         let uniqBalloon = new UniqBalloon('green', 30)
         this.balloons.push(uniqBalloon)
     }
 
-    static addAngryBalloon(){
+    static addAngryBalloon() {
         let angryBalloon = new AngryBalloon('black', 50)
         this.balloons.push(angryBalloon)
     }
-   
 
-    static checkIfBalloonBurst(){
+
+    static checkIfBalloonBurst() {
         this.balloons.forEach((balloon, index) => {
-            let distance = dist(balloon.x, balloon.y, mouseX,mouseY)
-            if(distance <= balloon.size / 2){
+            let distance = dist(balloon.x, balloon.y, mouseX, mouseY)
+            if (distance <= balloon.size / 2) {
                 balloon.burst(index)
             }
 
         }
-    )}
+        )
+    }
 }
 
-class CommonBalloon{
-    constructor(color,size){
+class CommonBalloon {
+    constructor(color, size) {
         this.x = random(width)
-        this.y = random (height - 10 , height + 50)
+        this.y = random(height - 10, height + 50)
         this.color = color;
         this.size = size;
     }
-    display(){
+    display() {
         fill(this.color);
         ellipse(this.x, this.y, this.size)
-        line(this.x, this.y + this.size / 2 , this.x, this.y + 2 * this.size)
+        line(this.x, this.y + this.size / 2, this.x, this.y + 2 * this.size)
     }
 
-    move(score){
-        if(score < 100) {
-            this.y -= 1 
-        }else if (score >= 100 && score <= 200){
+    move(score) {
+        if (score < 100) {
+            this.y -= 1
+        } else if (score >= 100 && score <= 200) {
             this.y -= 1.5
-        }else{
+        } else {
             this.y -= 2
         }
     }
 
 
-    burst(index){
+    burst(index) {
         Game.balloons.splice(index, 1)
         Game.score += 1
     }
 }
 
-class UniqBalloon extends CommonBalloon{
-    constructor(color,size){
-        super(color,size)
+class UniqBalloon extends CommonBalloon {
+    constructor(color, size) {
+        super(color, size)
     }
 
-    burst(index){
+    burst(index) {
         Game.balloons.splice(index, 1)
         Game.score += 10
     }
 }
 
-class AngryBalloon extends CommonBalloon{
-    constructor(color,size){
-        super(color,size)
+class AngryBalloon extends CommonBalloon {
+    constructor(color, size) {
+        super(color, size)
     }
 
-    burst(index){
+    burst(index) {
         Game.balloons.splice(index, 1)
         Game.score -= 10
     }
